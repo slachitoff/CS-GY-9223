2af1d17b3234e5e0a911528f112d2a22db889fba
slachitoff <73322777+slachitoff@users.noreply.github.com>
2023-12-14 21:24:26 -0500

    Update Clay.cs

--- Clay.cs
+++ Clay.cs
@@ -1,7 +1,7 @@
 /* Importantly, the Clay component in its entirety has been wholly adapted from the Wood script found here: 
 https://github.com/herbou/Unity_Pottery/blob/master/Assets/Scripts/Wood.cs, 
  where the original functionality of the Wood script found in the linked Github repository has been divided between the 
- Clay component shown below, and a separate Revolve component. Apart from changing some variable names, I had no contribution to the development of this component.*/
+ Clay component shown below, and a separate Revolve component. Apart from changing some variable names and values, I had no contribution to the development of this component.*/
 
 using System.Collections;
 using System.Collections.Generic;
